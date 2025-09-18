32e473427ecac97ace38ec49edd99abb5b3df6d6
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2025-09-18 13:48:43 -0400

    Revised firewall rules to use existing disabled rules.

--- windows/01-base/cd/common/configure-firewall.ps1
+++ windows/01-base/cd/common/configure-firewall.ps1
@@ -3,7 +3,5 @@ Write-Host "Configure Windows Firewall"
 
 Enable-NetFirewallRule -Group "@FirewallAPI.dll,-28752"
 Enable-NetFirewallRule -Group "@FirewallAPI.dll,-28782"
-New-NetFirewallRule -Action Allow -Direction Inbound -DisplayName 'ICMPv4 - Destination Unreachable In' -IcmpType '3' -Profile Any -Protocol ICMPv4
-New-NetFirewallRule -Action Allow -Direction Inbound -DisplayName 'ICMPv4 - Source Quench In' -IcmpType '4' -Profile Any -Protocol ICMPv4
-New-NetFirewallRule -Action Allow -Direction Inbound -DisplayName 'ICMPv4 - Echo Request In' -IcmpType '8' -Profile Any -Protocol ICMPv4
+Enable-NetFirewallRule -Group "@FirewallAPI.dll,-27000"
 Get-NetConnectionProfile | Set-NetConnectionProfile -NetworkCategory Private
