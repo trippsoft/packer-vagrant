0b442d151855cd9b04400db203bd9ecf091891ed
trippsc2 <46848743+trippsc2@users.noreply.github.com>
2025-09-29 12:25:17 -0400

    Addressed variable names.

--- .github/workflows/scripts/hyperv/build-base.sh
+++ .github/workflows/scripts/hyperv/build-base.sh
@@ -8,18 +8,20 @@ export TMPDIR='/mnt/c/Users/github-actions/AppData/Local/Temp'
 
 source ~/venv/ansible-2.16/bin/activate
 
+PACKER_VAR_FILE="./$1/01-base/$2.pkrvars.hcl"
+
 /usr/bin/packer build \
     -only "*.hyperv" \
-    -var-file="./${$1}/01-base/${$2}.pkrvars.hcl" \
+    -var-file="$PACKER_VAR_FILE" \
     -force \
     ./$1/01-base || \
 /usr/bin/packer build \
     -only "*.hyperv" \
-    -var-file="./${$1}/01-base/${$2}.pkrvars.hcl" \
+    -var-file="$PACKER_VAR_FILE" \
     -force \
     ./$1/01-base || \
 /usr/bin/packer build \
     -only "*.hyperv" \
-    -var-file="./${$1}/01-base/${$2}.pkrvars.hcl" \
+    -var-file="$PACKER_VAR_FILE" \
     -force \
     ./$1/01-base
--- .github/workflows/scripts/hyperv/build-cis.sh
+++ .github/workflows/scripts/hyperv/build-cis.sh
@@ -8,18 +8,20 @@ export TMPDIR='/mnt/c/Users/github-actions/AppData/Local/Temp'
 
 source ~/venv/ansible-2.16/bin/activate
 
+PACKER_VAR_FILE="./$1/02-cis/$2.pkrvars.hcl"
+
 /usr/bin/packer build \
     -only "*.hyperv" \
-    -var-file="./${$1}/02-cis/${$2}.pkrvars.hcl" \
+    -var-file="$PACKER_VAR_FILE" \
     -force \
     ./$1/02-cis || \
 /usr/bin/packer build \
     -only "*.hyperv" \
-    -var-file="./${$1}/02-cis/${$2}.pkrvars.hcl" \
+    -var-file="$PACKER_VAR_FILE" \
     -force \
     ./$1/02-cis || \
 /usr/bin/packer build \
     -only "*.hyperv" \
-    -var-file="./${$1}/02-cis/${$2}.pkrvars.hcl" \
+    -var-file="$PACKER_VAR_FILE" \
     -force \
     ./$1/02-cis
